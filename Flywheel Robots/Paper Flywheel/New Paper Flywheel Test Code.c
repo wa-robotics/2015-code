8ebfd40461fca0f549923f65aa81dd39b455e35c
Evan Strat <evan.strat@gmail.com>
2015-12-23 15:58:29 -0500

    Update motors/sensors setup

--- Flywheel Robots/Paper Flywheel/New Paper Flywheel Test Code.c
+++ Flywheel Robots/Paper Flywheel/New Paper Flywheel Test Code.c
@@ -1,5 +1,7 @@
+#pragma config(UART_Usage, UART1, uartVEXLCD, baudRate19200, IOPins, None, None)
+#pragma config(UART_Usage, UART2, uartNotUsed, baudRate4800, IOPins, None, None)
 #pragma config(I2C_Usage, I2C1, i2cSensors)
-#pragma config(Sensor, in1,    powerExpander,  sensorAnalog)
+#pragma config(Sensor, in1,    powerExpander,  sensorNone)
 #pragma config(Sensor, dgtl12, led,            sensorLEDtoVCC)
 #pragma config(Sensor, I2C_1,  ,               sensorQuadEncoderOnI2CPort,    , AutoAssign )
 #pragma config(Sensor, I2C_2,  ,               sensorQuadEncoderOnI2CPort,    , AutoAssign )
