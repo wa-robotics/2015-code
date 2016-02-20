73da514b3f311113bf1efc6121184d3195456eab
Evan Strat <evan10s@users.noreply.github.com>
2016-02-19 23:14:03 -0500

    Add driver control code (untested)

--- Flywheel Robots/State/Antimony/Faiz Flywheel with PID.c
+++ Flywheel Robots/State/Antimony/Faiz Flywheel with PID.c
@@ -28,6 +28,7 @@
 
 fw_controller lFly, rFly;
 string str;
+int flywheelMode = 0; //0 - stopped, 1 - close, 2 - center, 3 - purple, 4 - long
 #define FORWARD 1;
 #define BACKWARD -1;
 
@@ -41,9 +42,15 @@ void setRDriveMotors (float power) {
 	motor[rDriveBack] = power;
 }
 
+void setIntakeMotors (float power) {
+	motor[intakeLeft] = power;
+	motor[intakeRight] = power;
+}
+
+//work in progress
 void driveDistance (int encoderCounts, int direction, float power) {
 	int encoderGoalLeft = nMotorEncoder[lDriveFront] + encoderCounts*direction,
-			encoderGoalRight = nMotorEncoder[rDriveFront] + encoderCounts*direction;
+	encoderGoalRight = nMotorEncoder[rDriveFront] + encoderCounts*direction;
 
 	while (nMotorEncoder[lDriveFront] < encoderGoalLeft) {
 		if (encoderGoalLeft - nMotorEncoder[lDriveFront] <= 360) { //as the target is approached, start decreasing the power slightly
@@ -61,7 +68,7 @@ void driveDistance (int encoderCounts, int direction, float power) {
 //work in progress
 void rotate (int encoderCounts, int direction, float power) {
 	int encoderGoalLeft = nMotorEncoder[lDriveFront] + encoderCounts*direction,
-			encoderGoalRight = nMotorEncoder[rDriveFront] - encoderCounts*direction;
+	encoderGoalRight = nMotorEncoder[rDriveFront] - encoderCounts*direction;
 
 	while (nMotorEncoder[lDriveFront] < encoderGoalLeft) {
 		setLDriveMotors(power);
@@ -130,7 +137,7 @@ task leftFwControlTask()
 		//if(fw->current < fw->target - 20) {
 		//	setLeftFwSpeed( 70 );
 		//} else
-			setLeftFwSpeed(fw->motor_drive);
+		setLeftFwSpeed(fw->motor_drive);
 		str = sprintf( str, "%4d %4d  %5.2f", fw->target,  fw->current, nImmediateBatteryLevel/1000.0 );
 		displayLCDString(0, 0, str );
 		str = sprintf( str, "%4.2f %4.2f ", fw->drive, fw->drive_at_zero );
@@ -177,7 +184,7 @@ task rightFwControlTask()
 		//if(fw->current < fw->target - 20) {
 		//	setRightFwSpeed( 70 );
 		//} else
-			setRightFwSpeed( fw->motor_drive );
+		setRightFwSpeed( fw->motor_drive );
 
 		// Run at somewhere between 20 and 50mS
 		wait1Msec( FW_LOOP_SPEED );
@@ -218,12 +225,15 @@ void initializePIDPurple() {
 
 //stop flywheel
 void stopFlywheel() {
-	//disable PIC control of the flywheels
+	//disable PIC control of the flywheels and switch to open-loop control
 	stopTask(leftFwControlTask);
 	stopTask(rightFwControlTask);
+
 	//turn off the flywheel motors
 	setLeftFwSpeed(0);
 	setRightFwSpeed(0);
+
+	flywheelMode = 0; //make sure we know that the flywheel is stopped
 }
 
 task autonomous()
@@ -247,10 +257,24 @@ task autonomous()
 
 }
 
+task closeShootingMacro() {
+	while (1) {
+		if (vexRT[Btn8D] == 1 && flywheelMode == 1) { //only run this if the flywheel is in the correct operating state (close shooting only), to prevent mishaps resulting from accidental button presses
+			setIntakeMotors(115); //turn on the intake to outtake the balls
+			wait1Msec(1500); //wait long enough to shoot all the balls
+			setIntakeMotors(0); //stop the intake
+			stopFlywheel(); //turn off the flywheel
+		}
+
+		wait1Msec(25); //don't hog the CPU
+	}
+}
+
 int lSpeed = 60;
 int rSpeed = 60;
 task usercontrol()
 {
+	startTask(closeShootingMacro);
 	writeDebugStreamLine("nPgmTime,lFly.current, lFly.motor_drive, lFly.p, lFly.i, lFly.d, lFly.constant, 50*lFly.postBallLaunch, rFly.current, rFly.motor_drive, rFly.p, rFly.i, rFly.d, rFly.constant, 60*rFly.postBallLaunch");
 	//setLeftFwSpeed(lSpeed);
 	//setRightFwSpeed(rSpeed);
@@ -273,31 +297,70 @@ task usercontrol()
 	//FwVelocitySet(lFly,115,.7);
 	//FwVelocitySet(rFly,115,.7);
 
-	int intakePower;
-	int threshold = 15;
-	int lY;
-	int rY;
+	int intakePower,
+	threshold = 15,
+	lY,
+	rY;
 	while (true)
 	{
-		intakePower = 125*vexRT[Btn5U] - 125*vexRT[Btn5D];
-		intakePower = 115; //testing
-		motor[intakeLeft] = intakePower;
-		motor[intakeRight] = intakePower;
-
 		//drivetrain
 		lY = vexRT[Ch3];
 		rY = vexRT[Ch2];
-
 		motor[lDriveFront] = (abs(lY) > threshold) ? lY : 0;
 		motor[lDriveBack] = (abs(lY) > threshold) ? lY : 0;
 		motor[rDriveFront] = (abs(rY) > threshold) ? rY : 0;
 		motor[rDriveBack] = (abs(rY) > threshold) ? rY : 0;
 
+		//intake
+		intakePower = 125*vexRT[Btn5U] - 125*vexRT[Btn5D];
+		setIntakeMotors(intakePower);
 
-		//setLeftFwSpeed(lSpeed);
-		//setRightFwSpeed(rSpeed);
-		//writeDebugStreamLine("%d,%d,%d,%d,%d,%d,%d,%d",rFly.encoder_timestamp, rFly.e_current, rFly.error, rFly.current, rFly.motor_drive, rFly.p, rFly.i, rFly.d);
-	  writeDebugStreamLine("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d",nPgmTime,lFly.current, lFly.motor_drive, lFly.p, lFly.i, lFly.d, lFly.constant, 50*lFly.postBallLaunch, rFly.current, rFly.motor_drive, rFly.p, rFly.i, rFly.d, rFly.constant, 60*rFly.postBallLaunch);
-		wait1Msec(25);
+		//flywheel speed control
+		//7U - long, 7R - purple, 7D - short
+		//8R - stop, 8D - if short shooting (flywheel must be on and in short shooting mode), outtake 4 balls and then stop the flywheel automatically
+		if (vexRT[Btn7U] == 1 && flywheelMode != 4) { //second condition prevents reinitialization of long shooting if the flywheel is currently in long shooting mode
+			if (flywheelMode >= 1) { //if the flywheel is currently running (modes 1-4), we need to stop the controller tasks before re-initializing the PID controller
+				stopTask(leftFwControlTask);
+				stopTask(rightFwControlTask);
+			}
+
+			//next 4 lines have to run every time to run flywheel
+			flywheelMode = 4; //make sure we set the flywheel mode
+			initializePIDLong(); //prepare controller for long shooting
+			//set long shooting velocities
+		  FwVelocitySet(lFly,136,.7);
+	    FwVelocitySet(rFly,136,.7);
+		} else if (vexRT[Btn7R] == 1 && flywheelMode != 3) { //purple shooting
+			if (flywheelMode >= 1) { //if the flywheel is currently running (modes 1-4), we need to stop the controller tasks before re-initializing the PID controller
+				stopTask(leftFwControlTask);
+				stopTask(rightFwControlTask);
+			}
+
+			//next 4 lines have to run every time to run flywheel
+			flywheelMode = 3;
+			initializePIDPurple();
+			FwVelocitySet(lFly,115,.7);
+			FwVelocitySet(rFly,115,.7);
+
+		} else if (vexRT[Btn7D] == 1 && flywheelMode != 1) { //close shooting
+			if (flywheelMode >= 1) { //if the flywheel is currently running (modes 1-4), we need to stop the controller tasks before re-initializing the PID controller
+				stopTask(leftFwControlTask);
+				stopTask(rightFwControlTask);
+			}
+
+			//next 4 lines have to run every time to run flywheel
+			flywheelMode = 1;
+			initializePIDShort();
+			FwVelocitySet(lFly, 97.75, .5);
+			FwVelocitySet(rFly, 97.75, .5);
+
+		} else if (vexRT[Btn8R] == 1 && flywheelMode >= 1) { //this is an else statement so that if two buttons are pressed, we won't switch back and forth between starting and stopping the flywheel
+			stopFlywheel();																		 // flywheelMode needs to be greater than 1 so that we don't run the stopFlywheel function if the flywheel is already stopped
+		}
+
+
+		//writeDebugStreamLine("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d",nPgmTime,lFly.current, lFly.motor_drive, lFly.p, lFly.i, lFly.d, lFly.constant, 50*lFly.postBallLaunch, rFly.current, rFly.motor_drive, rFly.p, rFly.i, rFly.d, rFly.constant, 60*rFly.postBallLaunch);
+
+		wait1Msec(25); //don't overload the CPU
 	}
 }
