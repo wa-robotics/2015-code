4110ca01e2206cd424fa7025a1b3d53acc411502
Evan Strat <evan10s@users.noreply.github.com>
2016-02-22 13:20:44 -0500

    work on long shooting

--- Flywheel Robots/State/Antimony/Faiz Flywheel with PID.c
+++ Flywheel Robots/State/Antimony/Faiz Flywheel with PID.c
@@ -225,8 +225,8 @@ void initializePIDLong() {
 	//tbhInit(rFly, 392, 0.55, 0.008064, 0, 70); //initialize PID for right side of the flywheel
 	//note the order of the parameters:
 	//(controller, motor ticks per rev, KpNorm, KpBallLaunch, Ki, Kd, constant, RPM drop on ball launch)
-	tbhInit(lFly, 392, 0.2481, 0.6481, 0.005481, 0, 75, 20); //initialize PID for left side of the flywheel //left side might be able to have a higher P
-	tbhInit(rFly, 392, 0.2481, 0.6481, 0.005481, 0, 75, 20); //initialize PID for right side of the flywheel //x.x481
+	tbhInit(lFly, 392, 0.2481, 0.7481, 0.005481, 0, 75, 20); //initialize PID for left side of the flywheel //left side might be able to have a higher P
+	tbhInit(rFly, 392, 0.2481, 0.7481, 0.005481, 0, 75, 20); //initialize PID for right side of the flywheel //x.x481
 	startTask(leftFwControlTask);
 	startTask(rightFwControlTask);
 }
@@ -360,8 +360,8 @@ task usercontrol()
 			flywheelMode = 4; //make sure we set the flywheel mode
 			initializePIDLong(); //prepare controller for long shooting
 			//set long shooting velocities
-		  FwVelocitySet(lFly,136,.7);
-	    FwVelocitySet(rFly,136,.7);
+		  FwVelocitySet(lFly,132,.7);
+	    FwVelocitySet(rFly,132,.7);
 		} else if (vexRT[Btn7R] == 1 && flywheelMode != 3) { //purple shooting
 			if (flywheelMode >= 1) { //if the flywheel is currently running (modes 1-4), we need to stop the controller tasks before re-initializing the PID controller
 				stopTask(leftFwControlTask);
