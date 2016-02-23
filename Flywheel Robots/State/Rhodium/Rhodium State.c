0b46e8dcd19e72852901c05ec56d88fee3932df2
Evan Strat <evan10s@users.noreply.github.com>
2016-02-23 14:16:18 -0500

    work on short shooting

--- Flywheel Robots/State/Rhodium/Rhodium State.c
+++ Flywheel Robots/State/Rhodium/Rhodium State.c
@@ -207,8 +207,8 @@ void initializePIDLong() {
 void initializePIDShort() {
 	//note the order of the parameters:
 	//(controller, motor ticks per rev, KpNorm, KpBallLaunch, Ki, Kd, constant, RPM drop on ball launch)
-	tbhInit(lFly, 392, 0.7481, 1.0981, 0.005481, 0, 50, 20); //initialize PID for left side of the flywheel //left side might be able to have a higher P
-	tbhInit(rFly, 392, 0.7481, 1.0981, 0.005481, 0, 50, 20); //initialize PID for right side of the flywheel //x.x481
+	tbhInit(lFly, 392, 0.7481, 1.1981, 0.005481, 0, 42, 20); //initialize PID for left side of the flywheel //left side might be able to have a higher P
+	tbhInit(rFly, 392, 0.7481, 1.1981, 0.005481, 0, 42, 20); //initialize PID for right side of the flywheel //x.x481
 	startTask(leftFwControlTask);
 	startTask(rightFwControlTask);
 }
@@ -262,7 +262,7 @@ task closeShootingMacro() {
 	while (1) {
 		if (vexRT[Btn8D] == 1 && flywheelMode == 1) { //only run this if the flywheel is in the correct operating state (close shooting only), to prevent mishaps resulting from accidental button presses
 			userIntakeControl = false; //prevent user from controlling intake while macro is running
-			setIntakeMotors(112); //turn on the intake to outtake the balls
+			setIntakeMotors(105); //turn on the intake to outtake the balls
 			wait1Msec(1750); //wait long enough to shoot all the balls
 			setIntakeMotors(0); //stop the intake
 			userIntakeControl = true; //return intake control to user
@@ -285,8 +285,11 @@ task usercontrol()
 	//short shooting
 	//intake power 115
 	//initializePIDShort();
-	//FwVelocitySet(lFly, 97.75, .5);
-	//FwVelocitySet(rFly, 97.75, .5);
+	//FwVelocitySet(lFly, 93.5, .5);
+	//FwVelocitySet(rFly, 93.5, .5);
+	//wait1Msec(2000);
+	//userIntakeControl = false;
+	//setIntakeMotors(100);
 	//wait1Msec(2000); //testing
 	//long shooting
 	//initializePIDLong();
@@ -355,8 +358,8 @@ task usercontrol()
 			//next 4 lines have to run every time to run flywheel
 			flywheelMode = 1;
 			initializePIDShort();
-			FwVelocitySet(lFly, 97.75, .5);
-			FwVelocitySet(rFly, 97.75, .5);
+			FwVelocitySet(lFly, 93.5, .5);
+			FwVelocitySet(rFly, 93.5, .5);
 
 		} else if (vexRT[Btn8R] == 1 && flywheelMode >= 1) { //this is an else statement so that if two buttons are pressed, we won't switch back and forth between starting and stopping the flywheel
 			stopFlywheel();																		 // flywheelMode needs to be greater than 1 so that we don't run the stopFlywheel function if the flywheel is already stopped
